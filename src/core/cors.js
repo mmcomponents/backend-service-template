1b20d7af3429b67b4021411d696365a3b3337bcf
marcus <marcusmmourao@gmail.com>
2020-09-19 14:52:53 -0300

    fix: allow cors from every host BREAKING CHANGE: now cors allows connections from everywhere

--- src/core/cors.js
+++ src/core/cors.js
@@ -1,6 +1,6 @@
 function cors(req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_CORS_FROM);
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
